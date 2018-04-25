f4d9912db3c232369306c7692040ef00e0dcd70a
CHANG-CHING WANG <chinchin0207@gmail.com>
2018-04-24 20:48:50 -0700

    switch backend server to heroku

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  _serverurl = 'http://localhost:3000/';
-  // _serverurl = 'https://zebra-service.herokuapp.com/';
+  // _serverurl = 'http://localhost:3000/';
+  _serverurl = 'https://zebra-service.herokuapp.com/';
   constructor() { }
 
   get serverurl() {
